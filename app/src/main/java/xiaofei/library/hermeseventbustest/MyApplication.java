aa5b47c8099d37bdb66ca358ee42ded8e53bc39e
Xiaofei <xiaofei.it@gmail.com>
2016-07-04 18:17:23 +0800

    Find a potential bug

--- app/src/main/java/xiaofei/library/hermeseventbustest/MyApplication.java
+++ app/src/main/java/xiaofei/library/hermeseventbustest/MyApplication.java
@@ -30,5 +30,6 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         HermesEventBus.getDefault().init(this);
+        HermesEventBus.getDefault().post("Bug");
     }
 }
